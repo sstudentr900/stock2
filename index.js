752570c3e22cd400bdff43744c69c614dbebaeb9
John Doe <johndoe@example.com>
2022-09-06 22:47:57 +0800

    1

--- index.js
+++ index.js
@@ -59,19 +59,21 @@ function wordSave(userMessage,event){
     const array = userMessage.split(',')//[ '阿雞記關鍵字', '1', '2' ]
     const rows = await sheet.getRows();
     const question = array[1]
+    let message = '好的，我記住了'
     let answer = array[2]
     for (let [rowIndex, row] of rows.entries()) {
       // console.log(index, row)
-      // console.log('question',row['question']==question,row['question'],question)
+      // console.log('有沒有該question',row['question']==question,row['question'],question)
       if(row['question']==question){
         let rawData = row._rawData.slice()
         rawData.shift()
-        // console.log('answer==-1',rawData.indexOf(answer),rawData,answer)
+        // console.log('有沒有該answer(-1)',rawData.indexOf(answer),rawData,answer)
         if(rawData.indexOf(answer)==-1){
           // console.log('n repeat')
-          // console.log('length>4',rawData.length)
+          // console.log('該length>4',rawData.length)
           if(rawData.length>4){
             // console.log('more than 5')
+            message = '超過儲存數'
             answer = false
             break;
           }else{
@@ -105,57 +107,64 @@ function wordSave(userMessage,event){
     }
 
     //reply
-    // event.reply('好的，我記住了')
+    // event.reply(message)
   })
 }
 function wordSearch(userMessage,event){
-  for (elements of chickenSheetData) {
-    if(elements[0]==userMessage){
-      sendMessageFn([{
-        'type': 'text',
-        'text': elements[randomFn(elements.length)]
-      }],replyToken)
-      break;
+  googleSheetGetData('1813117258')
+  .then(async(sheet)=>{
+    const rows = await sheet.getRows();
+    let message = ''
+    for(let [index,row] of rows.entries()){
+      if(row['question']==userMessage){
+        let words = row._rawData.slice()
+        message = words[randomFn(words.length-1)]
+        break;
+      }
     }
-  }
+
+    console.log(message)
+    //reply
+    // event.reply(message)
+  })
 }
 
 // eateSearch()
 // eateSave('阿雞記吃的,龍點心')
 // wordSave('阿雞記關鍵字,1,3')
-wordSearch()
+// wordSearch(1)
 
 // 當有人傳送訊息給Bot時
-// bot.on('message', function (event) {
+bot.on('message', function (event) {
   //接收訊息訊息
-  // const userMessage = event.message.text;
-  // if(~userMessage.indexOf('阿雞記吃的')){  
-  //   eateSave(userMessage,event)
-  // }else if(~userMessage.indexOf('吃什麼')){
-  //   eateSearch(event)
-  // }else if(~userMessage.indexOf('阿雞記關鍵字')){
-  //   wordSave(userMessage)
-  // }else{
-  //   //亂數取阿雞表單資料
-  //   wordSearch(userMessage)
-  // }
+  const userMessage = event.message.text;
+  if(~userMessage.indexOf('阿雞記吃的')){  
+    eateSave(userMessage,event)
+  }else if(~userMessage.indexOf('吃什麼')){
+    eateSearch(event)
+  }else if(~userMessage.indexOf('阿雞記關鍵字')){
+    wordSave(userMessage,event)
+  }else{
+    //亂數取阿雞表單資料
+    wordSearch(userMessage,event)
+  }
 
-  //回傳方式
-  //location
-  // event.reply({
-  //   "type": "location",
-  //   "title": "my location",
-  //   "address": "1-6-1 Yotsuya, Shinjuku-ku, Tokyo, 160-0004, Japan",
-  //   "latitude": 35.687574,
-  //   "longitude": 139.72922
-  // })
-// });
+    //回傳方式
+    //location
+    // event.reply({
+    //   "type": "location",
+    //   "title": "my location",
+    //   "address": "1-6-1 Yotsuya, Shinjuku-ku, Tokyo, 160-0004, Japan",
+    //   "latitude": 35.687574,
+    //   "longitude": 139.72922
+    // })
+});
 
 
 
 // Bot所監聽的webhook路徑與port
-// bot.listen('/linewebhook',process.env.port || 80, function () {
-//     console.log('BOT已準備就緒');
-// });
+bot.listen('/linewebhook',process.env.port || 80, function () {
+    console.log('BOT已準備就緒');
+});
 
 
